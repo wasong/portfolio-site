c2c576c93402f53c7b3c768d43d09300343fe282
bChen-95 <122933241+bChen-95@users.noreply.github.com>
2023-06-16 04:53:02 +0000

    started about page

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import aboutImg from '../img/about.jpg';
+
+function About() {
+    
+}
+
+return (
+    <div className="about">
+        <h1>About Me</h1>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sit. Alias quibusdam quam voluptatibus omnis illo numquam ab cupiditate possimus sapiente pariatur, consectetur, ea voluptate in dolor! Libero, ipsa dignissimos.</p>
+    </div>
+    <img
+)
