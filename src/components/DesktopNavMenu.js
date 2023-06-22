46470341d1acd2c843e290a1234410642c7cb623
Brice Chen <bchen72@my.bcit.ca>
2023-06-22 13:19:59 -0700

    uncommented pagenotfound on approuter

--- src/components/DesktopNavMenu.js
+++ src/components/DesktopNavMenu.js
@@ -3,7 +3,7 @@ import { homeIconWhiteBg } from "../globals/globals"
 function DesktopNavMenu() {
 
     return (
-        <nav className="desktop-nav-menu">
+        <nav className="desktop-nav-menu hidden md:absolute block ">
             <ul>
                 <li><img src={homeIconWhiteBg} alt="Home" /></li>   
             </ul>
