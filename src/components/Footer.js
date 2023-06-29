4663e9f1f81855fab4b87006e59567258f269b0c
Brice Chen <bchen72@my.bcit.ca>
2023-06-29 09:31:43 -0700

    something on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
 return(
 <footer>
     <div>
-        <p className='text-center' >&#169; Brice Chen 2023</p>
+        <p className='text-center absolute bottom=' >&#169; Brice Chen 2023</p>
     </div>
 </footer>
 
