f241b49b8ae0567fa2a564a9e4bdaefe13d3223f
Brice Chen <bchen72@my.bcit.ca>
2023-07-16 14:16:57 -0700

    added margin to toolkit part

--- src/components/About.js
+++ src/components/About.js
@@ -4,19 +4,21 @@ function About() {
     
 
 return (
-    <section id="about">
+    <section id="about" className="my-20"> 
         <article className="about">
-            <h2  className= "text-center text-2xl"> About Me</h2>
+            <h2  className= "text-center text-3xl lg:text-4xl"> About Me</h2>
             <img  src="https://i.imgur.com/Yhp8tZB.jpg" alt="Brice Chen" className="mx-auto w-[250px]" />
             <p className= "my-4 mx-8"> I am front-end web developer with a background in nursing. I enjoy playing basketball, riding motorcycles, and hiking. I also like traveling but my wallet is holding me back. </p>
         </article>
 
         <article>
-            <h2 className= "text-center text-2xl"> Toolkit</h2>
-            <h3>Development</h3>
-            <p> HTML, CSS, JavaScript, React, SASS</p>
-            <h3>Design</h3>
-            <p>Figma, Adobe XD</p>
+            <h2 className= "text-center text-2xl mt-8"> Toolkit</h2>
+            <div className="skills-wrapper mx-8">
+                <h3>Development</h3>
+                    <p> HTML, CSS, JavaScript, React, SASS</p>
+                <h3>Design</h3>
+                <p>Figma, Adobe XD</p>
+            </div>
         </article>
         
     </section>
