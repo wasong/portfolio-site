47ff78da6fe19523c3ec6e452a0feaa9b6ba1d3a
Brice Chen <brice_chen@hotmail.com>
2023-07-20 15:53:42 -0700

    added flex for different desktop styling for about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,21 +5,28 @@ function About() {
 
 return (
     <section id="about" className="min-h-screen flex flex-col mx-auto reveal my-16"> 
-        <div className="aboutText">
-            <h2  className= "text-center text-3xl lg:text-5xl my-8 font-bold"> About Me</h2>
-            <img  src="https://i.imgur.com/Yhp8tZB.jpg" alt="Brice Chen" className="mx-auto w-[250px]" />
-            <p className= "my-4 mx-auto max-w-[500px] lg:text-lg     "> I am front-end web developer with a background in nursing. I enjoy playing basketball, riding motorcycles, and hiking. I also like traveling but my wallet is holding me back. </p>
-        </div>
+        
+        <h2  className= "text-center text-3xl lg:text-5xl my-8 font-bold"> About Me</h2>
+            <div className="about-flex-container"> 
+                    <img  
+                    src="https://i.imgur.com/Yhp8tZB.jpg" 
+                    srcset=" " 
+                    alt="Brice Chen" 
+                    className="mx-auto w-[250px]" />
+                
+                <div className="aboutText">
+                    <p className= "my-4 mx-auto max-w-[500px] lg:text-lg     "> I am front-end web developer with a background in nursing. I enjoy playing basketball, riding motorcycles, and hiking. I also like traveling but my wallet is holding me back. </p>
+                
+                    <h2 className= "text-center text-2xl mt-8 font-bold lg:text-3xl"> Toolkit</h2>
+                    <div className="skills-wrapper my-8 mx-auto max-w-[500px]">
+                        <h3 className="font-bold lg:text-lg">Development</h3>
+                            <p> HTML, CSS, JavaScript, React, SASS, PHP, WordPress</p>
+                        <h3 className="font-bold mt-4 lg:text-lg">Design</h3>
+                        <p>Figma, Adobe XD</p>
+                    </div>
+                </div>
 
-        <div className="about">
-            <h2 className= "text-center text-2xl mt-8 font-bold lg:text-3xl"> Toolkit</h2>
-            <div className="skills-wrapper my-8 mx-auto max-w-[500px]">
-                <h3 className="font-bold lg:text-lg">Development</h3>
-                    <p> HTML, CSS, JavaScript, React, SASS, PHP, WordPress</p>
-                <h3 className="font-bold mt-4 lg:text-lg">Design</h3>
-                <p>Figma, Adobe XD</p>
             </div>
-        </div>
         
     </section>
 )
