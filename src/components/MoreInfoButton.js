062a952dc16c7b1e250cdcaa4f6c98e5fb2de58c
Brice Chen <bchen72@my.bcit.ca>
2023-07-20 21:17:14 -0700

    added sr-only and span to more info links

--- src/components/MoreInfoButton.js
+++ src/components/MoreInfoButton.js
@@ -4,7 +4,7 @@ function MoreInfoButton({project}) {
 
   return (
     <div className="more-info-button">
-      <Link to={`projects/${project.id}`} className="text-center block">More Info</Link>
+      <Link to={`projects/${project.id}`} className="text-center block">More Info<span className="sr-only">link to more information on project</span></Link>
     </div>
   )
 }
