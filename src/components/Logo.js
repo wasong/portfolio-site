feba9fe9a1757ffbb7fc1009f48b6e13737c6c3f
Brice Chen <bchen72@my.bcit.ca>
2023-07-20 19:36:41 -0700

    adjusted spacing of code

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,10 +3,10 @@ import logo from '../images/portfolio-logo.png';
 function Logo() {
     return (
         <span className='logo'>
-      <a href= "/">
-        <img src={logo} alt="logo" className="w-10 h-auto m-2 fixed bottom-0 left-0 z-[1003] lg:top-0 lg:fixed"></img>
-      </a>
-    </span>
+          <a href= "/">
+            <img src={logo} alt="logo" className="w-10 h-auto m-2 fixed bottom-0 left-0 z-[1003] lg:top-0 lg:fixed"></img>
+          </a>
+        </span>
     )
 }
 
