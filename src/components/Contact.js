b6ff1d2be2812065fb7389ebb129febea38f16e0
Brice Chen <bchen72@my.bcit.ca>
2023-07-25 16:11:53 -0700

    adjusted code layout in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,39 +11,39 @@ import emailIcon2 from '../images/emailIcon2.png';
 
 function Contact() {
     return (
-        <section id="contact" className="my-16">
-          <div className='reveal-wrapper'>
+      <section id="contact" className="my-16">
+        <div className='reveal-wrapper'>
           <h2 className="text-3xl text-center font-bold lg:text-5xl reveal ">Connect With Me</h2>
+        </div>
+        <div className='reveal-wrapper'>
+          <div className="socials-icon-container w-full inline-flex justify-center reveal">
+
+            <a href="https://github.com/bChen-95"  target="_blank" rel="noopener noreferrer" className="mx-2 my-4">
+              <HovImage
+                src={gitHubIcon}
+                alt="Github Icon"
+                hoverSrc={gitHubHov}
+              />
+            </a>
+
+            <a href="mailto:brice_chen@hotmail.com" target="_blank" rel="noopener noreferrer" className="mx-2 my-4">
+              <HovImage
+                src={emailIcon2}
+                alt="Email Icon"
+                hoverSrc={emailHov}
+              />
+            </a>
+
+            <a href="https://linkedin.com/in/bricechen" target="_blank" rel="noopener noreferrer" className="mx-2 my-4">
+              <HovImage
+                src={linkedinIcon}
+                alt="LinkedIn Icon"
+                hoverSrc={linkedinHov}
+              />
+            </a>
           </div>
-          <div className='reveal-wrapper'>
-         <div className="socials-icon-container w-full inline-flex justify-center reveal">
-
-        <a href="https://github.com/bChen-95"  target="_blank" rel="noopener noreferrer" className="mx-2 my-4">
-          <HovImage
-            src={gitHubIcon}
-            alt="Github Icon"
-            hoverSrc={gitHubHov}
-          />
-        </a>
-
-        <a href="mailto:brice_chen@hotmail.com" target="_blank" rel="noopener noreferrer" className="mx-2 my-4">
-          <HovImage
-            src={emailIcon2}
-            alt="Email Icon"
-            hoverSrc={emailHov}
-          />
-        </a>
-
-         <a href="https://linkedin.com/in/bricechen" target="_blank" rel="noopener noreferrer" className="mx-2 my-4">
-          <HovImage
-            src={linkedinIcon}
-            alt="LinkedIn Icon"
-            hoverSrc={linkedinHov}
-          />
-        </a>
         </div>
-        </div>
-        </section>
+      </section>
 )}
 
 export default Contact;
