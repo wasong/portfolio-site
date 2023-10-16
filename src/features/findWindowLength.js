f241a1c6305a6e009eb94efece2caf328f124be9
Brice Chen <brice_chen@hotmail.com>
2023-10-16 12:31:50 -0700

    remov useless js

--- src/features/findWindowLength.js
+++
@@ -1,8 +0,0 @@
-  // Get the window width and set it as a CSS variable
-  document.documentElement.style.setProperty('--window-width', `${window.innerWidth}px`);
-
-  // Update the variable on window resize
-  window.addEventListener('resize', () => {
-    document.documentElement.style.setProperty('--window-width', `${window.innerWidth}px`);
-  });
-
